a4c72f01e76aeb27ccc937bdf7243b1ff2e6ebf3
Elias Eriksson <eliaseriksson95@gmail.com>
2025-09-14 22:51:14 +0200

    fetching user

--- authentic/gateway/routes/users/controller.py
+++ authentic/gateway/routes/users/controller.py
@@ -5,23 +5,33 @@ import litestar
 from litestar import Request, Response
 from litestar.exceptions.http_exceptions import ClientException, NotFoundException
 from litestar.params import Parameter
-from sqlalchemy.exc import IntegrityError
+from sqlalchemy.exc import IntegrityError, NoResultFound
 
 from authentic import database, schemas
 
 
 class Controller(litestar.Controller):
     @litestar.get()
-    async def list(self, database: database.Client) -> List[schemas.User]:
+    async def list(self, database: database.Client) -> Response[List[schemas.User]]:
         users = await database.users.list()
-        return [schemas.User.from_model(user) for user in users]
+        return Response([schemas.User.from_model(user) for user in users])
+
+    @litestar.get("/{id:uuid}")
+    async def fetch(
+        self, database: database.Client, id: UUID
+    ) -> Response[schemas.User]:
+        try:
+            user = await database.users.fetch_by_key(id)
+        except NoResultFound:
+            raise NotFoundException("No such user.")
+        return Response(schemas.User.from_model(user))
 
     @litestar.post()
     async def create(
         self, database: database.Client, data: schemas.user.Creatable
-    ) -> schemas.User:
+    ) -> Response[schemas.User]:
         try:
             user = await database.users.create(data)
         except IntegrityError:
             raise ClientException("Email already in use.")
-        return schemas.User.from_model(user)
+        return Response(schemas.User.from_model(user))
