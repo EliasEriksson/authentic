739f25fc7304e44e69cd6cc97dc717e489e17aae
Elias Eriksson <eliaseriksson95@gmail.com>
2025-09-16 21:22:29 +0200

    improvements

--- authentic/database/client/operator.py
+++ authentic/database/client/operator.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 import asyncio
 import contextlib
-from contextlib import nullcontext
 from typing import *
 
 from sqlalchemy import Select, Sequence
@@ -13,24 +12,17 @@ from ..models.base import Model
 
 
 class Operator:
-    _lock: asyncio.Lock
     _reader: AsyncSession
     _writer: AsyncSession
 
     def __init__(self, reader: AsyncSession, writer: AsyncSession) -> None:
-        self._lock = asyncio.Lock()
         self._reader = reader
         self._writer = writer
 
     async def list[T: Model](self, query: Select[Tuple[T]]) -> Sequence[T]:
         result = await self._reader.execute(query)
         models = result.unique().scalars().all()
-        transaction = (
-            nullcontext(self._writer)
-            if self._writer.in_transaction()
-            else self.transaction()
-        )
-        async with transaction as session:
+        async with self.transaction(merge=False) as session:
             return await asyncio.gather(
                 *[
                     asyncio.create_task(session.merge(model, load=False))
@@ -43,44 +35,37 @@ class Operator:
         model = result.scalars().first()
         if model is None:
             raise NoResultFound("No row was found when one was required")
-        transaction = (
-            nullcontext(self._writer)
-            if self._writer.in_transaction()
-            else self.transaction()
-        )
-        async with transaction as session:
+        async with self.transaction(merge=False) as session:
             return await session.merge(model, load=False)
 
-    async def add[T: Model](
-        self, *models: T, session: AsyncSession | None = None
-    ) -> Sequence[T]:
-        transaction = nullcontext(session) if session else self.transaction()
-        async with transaction as session:
+    async def add[T: Model](self, *models: T) -> Sequence[T]:
+        async with self.transaction() as session:
             for model in models:
                 session.add(model)
         return models
 
-    async def delete[T: Model](
-        self, *models: T, session: AsyncSession | None = None
-    ) -> Sequence[T]:
-        transaction = nullcontext(session) if session else self.transaction()
-        async with transaction as session:
+    async def delete[T: Model](self, *models: T) -> Sequence[T]:
+        async with self.transaction() as session:
             result = await asyncio.gather(*[session.delete(model) for model in models])
         return result
 
     @contextlib.asynccontextmanager
-    async def transaction(self) -> AsyncIterator[AsyncSession]:
-        async with self._lock:
-            async with self._writer.begin():
-                yield self._writer
-                targets = [
-                    *(model for model in self._writer.new),
-                    *(model for model in self._writer.dirty),
-                    *(model for model in self._writer.deleted),
-                ]
-            await asyncio.gather(
-                *[
-                    asyncio.create_task(self._reader.merge(target, load=False))
-                    for target in targets
-                ]
-            )
+    async def transaction(self, *, merge=True) -> AsyncIterator[AsyncSession]:
+        if self._writer.in_transaction():
+            yield self._writer
+            return
+        async with self._writer.begin():
+            yield self._writer
+            targets = [
+                *(model for model in self._writer.new),
+                *(model for model in self._writer.dirty),
+                *(model for model in self._writer.deleted),
+            ]
+        if not merge:
+            return
+        await asyncio.gather(
+            *[
+                asyncio.create_task(self._reader.merge(target, load=False))
+                for target in targets
+            ]
+        )
