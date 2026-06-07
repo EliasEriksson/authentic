30b6a890a1836ad479575f96997f9ff7aabf7124
Elias Eriksson <eliaseriksson95@gmail.com>
2026-06-07 16:25:07 +0200

    work

--- client/src/components/common/icons/logo/index.tsx
+++ client/src/components/common/icons/logo/index.tsx
@@ -14,23 +14,35 @@ export function LogoIcon(props: LogoIcon.Props) {
       viewBox="0 0 512 512"
       {...props}
       className={css(styles.logo, props.className)}
+      strokeWidth={0}
     >
       <defs>
         <mask id={maskId}>
-          <rect x={80} y={192} width={352} height={352} fill={"white"} />
+          <rect
+            x={80}
+            y={192}
+            width={352}
+            height={352}
+            fill={"white"}
+            strokeWidth={0}
+          />
           <path
             d={"M 237,376 L 275,376 L 260,317 L 252,317 Z"}
+            stroke={"white"}
+            strokeWidth={0}
             fill={"black"}
           />
         </mask>
       </defs>
       <path
-        className={styles.foreground}
+        className={styles.lock}
+        strokeWidth={0}
         d="M420 192h-68v-80a96 96 0 10-192 0v80H92a12 12 0 00-12 12v280a12 12 0 0012 12h328a12 12 0 0012-12V204a12 12 0 00-12-12zm-106 0H198v-80.75a58 58 0 11116 0z"
       />
       <path
         className={css(styles.letter)}
         mask={`url(#${maskId})`}
+        strokeWidth={0}
         d={
           "M 140,466 L 210,466 L 222,425 L 290,425 L 302,466 L 372,466 L 285,234 L 227,234 Z"
         }
